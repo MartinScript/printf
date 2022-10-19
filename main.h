bb801a233c0d70a8c61df1d79319e00d6180c711
Martins Ojo <martinsojoola2@gmail.com>
2022-10-19 03:14:39 +0100

    correct mispelt  function

--- main.h
+++ main.h
@@ -19,7 +19,7 @@ void strev(char *str);
 char *fromDeci(unsigned int inputNum, int base);
 char reVal(int num);
 char *string_toupper(char *str);
-int print_unsigined_number(unsigned int n);
+int print_unsigned_number(unsigned int n);
 void print_memory(const void *ptr);
 unsigned char *_memcpy(unsigned char *dest, char *src, unsigned int n);
 
